a84511a1058a6c11ecbfc23f584e7d1ddea4784e
skt-t1-byungi <tiniwz@gmail.com>
2018-05-15 23:37:29 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,31 @@
 export type Action = () => Promise<any>
-export type PromiseOrAction = Promise<any> | Action
+export type Awaitable = Promise<any> | Action | Runner
 
 export class Runner {
   private _pList: Array<Promise<any>> = []
-  private _queue: Action[] = []
+  private _queue: Array<Action | Runner> = []
 
-  public run (...pList: PromiseOrAction[]) {
+  public run (...pList: Awaitable[]) {
     this._pList.push(
-      ...pList.map(p => typeof p === 'function' ? p() : p)
+      ...pList.map(p => p instanceof Runner ? p.waitAll() : typeof p === 'function' ? p() : p)
     )
   }
 
-  public queue (action: Action) {
+  public queue (action: Array<Action | Runner>) {
     if (typeof action !== 'function') {
-      throw new TypeError('The argument is not a function type')
+      throw new TypeError('Expected argument of type "function" or "Runner"')
     }
     this._queue.push(action)
   }
 
-  public runAll () {
-    this._pList.push(...this._queue.map(action => action()))
+  public waitAll () {
+    this._pList.push(...this._queue.map(q => q instanceof Runner ? q.waitAll() : q()))
     this._queue = []
 
     const pList = [...this._pList]
+    const clear = () => (this._pList = this._pList.filter(p => pList.indexOf(p) === -1))
 
-    return Promise.all(pList).then(() => {
-      this._pList = this._pList.filter(p => pList.indexOf(p) === -1)
-    })
+    return Promise.all(pList).then(clear).catch(clear)
   }
 }
 
